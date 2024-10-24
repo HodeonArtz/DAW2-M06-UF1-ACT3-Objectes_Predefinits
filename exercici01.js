f0c649f21d88a7065842dd6816e47328000a9936
HodeonArtz <martidicianolold@gmail.com>
2024-10-24 18:04:53 +0200

    #1 button generates random number implemented x2

--- exercici01.js
+++ exercici01.js
@@ -1,2 +1,9 @@
 "use strict";
-const number = Math.abs(Math.random() * 10);
+const btnPlay = document.querySelector(".btn-play");
+
+let number;
+
+btnPlay.addEventListener("click", () => {
+  number = Math.round(Math.random() * 10);
+  console.log(number);
+});
