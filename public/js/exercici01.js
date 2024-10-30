e6f9e76aeefb7dd3ed9c70b649befa2a7150cce1
HodeonArtz <martidicianolold@gmail.com>
2024-10-30 15:15:20 +0100

    #1 open window added

--- public/js/exercici01.js
+++ public/js/exercici01.js
@@ -24,4 +24,10 @@ function handleBtnPlayColor() {
 btnPlay.addEventListener("click", () => {
   number = Math.round(Math.random() * 10);
   handleBtnPlayColor();
+  window.open(
+    "./encerta.html",
+    undefined,
+    `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+width=100,height=100,left=0,top=0`
+  );
 });
