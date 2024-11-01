f3145d02354b61b9a24238d72e87f6759df0a478
HodeonArtz <martidicianolold@gmail.com>
2024-11-01 23:56:26 +0100

    #2 countdown implemented

--- public/js/exercici02.js
+++ public/js/exercici02.js
@@ -1,1 +1,10 @@
 "use strict";
+const countDownElement = document.querySelector(".count-down"),
+  countDownTimeStart = 3;
+
+let countDownTime = countDownTimeStart;
+
+let countDownInterval = setInterval(() => {
+  countDownElement.textContent = --countDownTime;
+  if (countDownTime <= 0) clearInterval(countDownInterval);
+}, 1000);
