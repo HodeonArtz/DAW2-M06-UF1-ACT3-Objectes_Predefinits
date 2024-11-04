ed28561e7526a5dc419fbe2d0c9103fb3640f339
HodeonArtz <martidicianolold@gmail.com>
2024-11-04 16:08:10 +0100

    #2 window click logic implemented

--- public/js/exercici02.js
+++ public/js/exercici02.js
@@ -1,14 +1,15 @@
 "use strict";
+
 const countDownElement = document.querySelector(".count-down"),
   countDownTimeStart = 30,
   windowWidthPx = 320,
   windowHeightPx = 160,
   backgroundColors = ["Amarillo", "Verde", "Cian", "Morado"];
 
-const windows = [];
-
-let countDownTime = countDownTimeStart,
-  totalWindowsOpened = 0;
+let windows = [],
+  countDownTime = countDownTimeStart,
+  totalWindowsOpened = 0,
+  firstClickedWindow = null;
 
 let countDownInterval = setInterval(() => {
   // View countdown
@@ -25,11 +26,9 @@ function random(min, max) {
 function setRandomBgColor(colorWindow) {
   const randomColor =
     backgroundColors[Math.round(random(0, backgroundColors.length - 1))];
-  console.log(randomColor);
 
   colorWindow.window.top.document.title = randomColor;
   colorWindow.document.body.classList.add(randomColor.toLowerCase());
-  console.log(colorWindow.document);
   // setting this class automatically changes window's color
   colorWindow.document.querySelector(".color-name").textContent = randomColor;
 }
@@ -44,15 +43,61 @@ function openNewColorWindow(
     `fullscreen=no,height=${windowHeightPx},width=${windowWidthPx},resizable=no,titlebar=yes,
     left=${posX - windowWidthPx / 2},top=${posY - windowHeightPx / 2}`
   );
-  windows.push(newWindow);
 
-  newWindow.addEventListener("load", () => {
-    setRandomBgColor(newWindow);
-    totalWindowsOpened++;
+  try {
+    newWindow.addEventListener("load", () => {
+      setRandomBgColor(newWindow);
+      totalWindowsOpened++;
+      newWindow.addEventListener("click", () => {
+        // mostrar animación de seleccionado
+        if (newWindow.document.hasFocus) handleWindowClick(newWindow);
+      });
+      windows.push(newWindow);
+    });
+  } catch (error) {
+    alert(
+      "Activa el permiso de ventanas emergentes para esta página para que funcione el juego."
+    );
+    console.error(
+      "Activa el permiso de ventanas emergentes para esta página para que funcione el juego."
+    );
+    console.error(error);
+  }
+}
+
+function closeColorWindow(...colorWindows) {
+  colorWindows.forEach((colorWindow) => {
+    console.log(colorWindow);
+    colorWindow.close();
+    windows = windows.filter((openedWindow) => openedWindow !== colorWindow);
   });
 }
 
-function closeColorWindow(colorWindow) {
-  colorWindow.close();
-  totalWindowsOpened--;
+function handleWindowClick(clickedWindow) {
+  if (!firstClickedWindow) {
+    console.log("First click");
+    firstClickedWindow = clickedWindow;
+    return;
+  }
+
+  const firstColorName =
+      firstClickedWindow.document.querySelector(".color-name").textContent,
+    secondColorName =
+      clickedWindow.document.querySelector(".color-name").textContent;
+
+  if (firstColorName !== secondColorName) {
+    console.log("Second click: colors are not the same");
+    firstClickedWindow = null;
+    return;
+  }
+
+  if (firstClickedWindow !== clickedWindow) {
+    console.log("Second click: windows are different");
+    closeColorWindow(firstClickedWindow, clickedWindow);
+    firstClickedWindow = null;
+    return;
+  }
+  console.log("Second click: same window clicked");
+  openNewColorWindow();
+  firstClickedWindow = null;
 }
