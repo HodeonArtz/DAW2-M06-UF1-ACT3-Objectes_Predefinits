60736242848e0c14217f45eb59988f8b8c17c6fa
HodeonArtz <martidicianolold@gmail.com>
2024-11-21 19:00:54 +0100

    JSDoc Countdown

--- public/js/exercici02/Countdown.js
+++ public/js/exercici02/Countdown.js
@@ -1,12 +1,40 @@
+/**
+ * Crea un cuenta atrás de manera asíncrona.
+ */
 export default class Countdown {
+  /**
+   * Referencia al Interval del cuenta atrás.
+   *
+   * @type {number | null}
+   */
   #interval = null;
+
+  /**
+   * Tiempo inicio del cuenta atrás.
+   *
+   * @type {number}
+   */
   #countdownTime = 0;
+
+  /**
+   * Tiempo actual del cuenta atrás.
+   * @type {number}
+   */
   #remainingTime = 0;
 
+  /**
+   *
+   * @param {number} countdownTime Indica el tiempo inicio del cuenta atrás
+   */
   constructor(countdownTime) {
     this.#countdownTime = countdownTime;
     this.#remainingTime = countdownTime;
   }
+
+  /**
+   * Detiene el cuenta atrás y lo deja en el tiempo inicial
+   * @see {@link Countdown.#countdownTime}
+   */
   stop() {
     if (this.#interval) {
       clearInterval(this.#interval);
@@ -14,6 +42,11 @@ export default class Countdown {
     }
     this.#remainingTime = this.#countdownTime;
   }
+  /**
+   *  Inicia el contador
+   * @param {function} handleOnUpdate función que se ejecuta en cada intervalo
+   * @param {function} handleOnEnd función que se ejecuta cuando termina el cuenta atrás
+   */
   start(handleOnUpdate, handleOnEnd) {
     const pastTime = Date.now();
     this.#interval = setInterval(() => {
@@ -36,6 +69,13 @@ export default class Countdown {
       this.stop();
     }, 50);
   }
+
+  /**
+   * Consigue el tiempo actual del cuenta atrás en segundos.milisegundos
+   *
+   * @readonly
+   * @type {number}
+   */
   get remainingTime() {
     return this.#remainingTime.toFixed(2);
   }
